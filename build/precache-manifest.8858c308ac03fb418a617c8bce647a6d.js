b86d642f6e5d9f4abae2e723df425c7fad6ca2cf
Antonio Gargaro <antoniogargaro97@gmail.com>
2019-10-20 10:50:38 +0100

    production build

---
+++ build/precache-manifest.8858c308ac03fb418a617c8bce647a6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf77a3aa1cd21defd0c2ab471fdff66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34f742e2f406d58c0089",
+    "url": "/static/css/main.d583d5c5.chunk.css"
+  },
+  {
+    "revision": "c982544d52e40aaffb40",
+    "url": "/static/js/2.e01ad54f.chunk.js"
+  },
+  {
+    "revision": "34f742e2f406d58c0089",
+    "url": "/static/js/main.0a76861a.chunk.js"
+  },
+  {
+    "revision": "b094d18a6b68fe81e89f",
+    "url": "/static/js/runtime-main.184c5806.js"
+  }
+]);
